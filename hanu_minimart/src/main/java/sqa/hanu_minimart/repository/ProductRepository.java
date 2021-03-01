7147d9bb9ba3dcf1796e05d9af0e676fda8b8705
hakunhan <hakunhan@gmail.com>
2021-03-01 20:24:51 +0700

    Update CRUD and esential method for product

--- hanu_minimart/src/main/java/sqa/hanu_minimart/repository/ProductRepository.java
+++ hanu_minimart/src/main/java/sqa/hanu_minimart/repository/ProductRepository.java
@@ -1,10 +1,20 @@
 package sqa.hanu_minimart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sqa.hanu_minimart.model.Product;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    List<Product> findByName(String name);
+
+    @Query(value = "SELECT * FROM product GROUP BY name ORDER BY import_date DESC ", nativeQuery = true)
+    List<Product> findNewestImportProduct();
+
+    @Query(value = "SELECT * FROM product WHERE expire_date BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 90 DAY)", nativeQuery = true)
+    List<Product> findNearlyExpireProduct();
 }
