6390f16a877f622c42918fcda721b31dd9710cd5
Vu1911 <1801040236@s.hanu.edu.vn>
2021-03-03 15:10:28 +0700

    ok

--- hanu_minimart/src/main/java/sqa/hanu_minimart/config/ProductConfig.java
+++ hanu_minimart/src/main/java/sqa/hanu_minimart/config/ProductConfig.java
@@ -18,19 +18,12 @@ public class ProductConfig {
         return args -> {
             Product bread = new Product("Bread", 2000, 2, "FOOD", LocalDate.now());
             Product candy = new Product("Candy", 10000, 10, "FOOD", LocalDate.now());
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> 6a5b5810694950d60ff1c9a01ade486bdaebe1fe
             List item = new ArrayList();
             item.add(bread);
             item.add(candy);
             productRepository.saveAll(item);
-<<<<<<< HEAD
 
-=======
->>>>>>> 6a5b5810694950d60ff1c9a01ade486bdaebe1fe
         };
     }
 }
