af1128a2140d69c197535234ffeac072d36ba62e
Vu1911 <1801040236@s.hanu.edu.vn>
2021-03-09 18:19:28 +0700

    Vu Pagination

---
+++ hanu_minimart/src/main/java/sqa/hanu_minimart/controller/AccountController.java
@@ -0,0 +1,57 @@
+package sqa.hanu_minimart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sqa.hanu_minimart.model.User;
+import sqa.hanu_minimart.payload.LoginRequest;
+import sqa.hanu_minimart.payload.SignUpRequest;
+import sqa.hanu_minimart.repository.UserRepository;
+import sqa.hanu_minimart.service.AccountService;
+import sqa.hanu_minimart.service.AuthenService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(path="/getAll")
+    public ResponseEntity<?> getAllAccount( @RequestParam(required = false) Long id,
+                                               @RequestParam(required = false, defaultValue = "_") String name,
+                                               @RequestParam(required = false) String username,
+                                               @RequestParam(required = false) String phoneNumber,
+                                               @RequestParam(required = false, defaultValue = "_") String address,
+                                               @RequestParam(defaultValue = "0") int page,
+                                               @RequestParam(defaultValue = "10") int size,
+
+                                               @RequestParam(defaultValue = "id,desc") String[] sort) {
+
+        try {
+            return new ResponseEntity<>(accountService.getAllAccount(id, username, phoneNumber, name, address, page, size, sort ), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PostMapping(path="/update/{id}")
+    public ResponseEntity<?> upadateAccount (@RequestParam Long id, @Valid @RequestBody User request){
+        return new ResponseEntity<>(accountService.updateUser(id, request), HttpStatus.OK);
+    }
+
+    @GetMapping(path="/delete/{id}")
+    public ResponseEntity<?> deleteAccount (@RequestParam Long id){
+        accountService.deleteUser(id);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+
+}
---
+++ hanu_minimart/src/main/java/sqa/hanu_minimart/service/AccountService.java
@@ -0,0 +1,89 @@
+package sqa.hanu_minimart.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import sqa.hanu_minimart.model.User;
+import sqa.hanu_minimart.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private UserRepository userRepository;
+    public Page<User> getAllAccount(Long id, String username, String phoneNumber, String name, String address, int page, int size, String[] sort){
+
+        List<Order> orders = new ArrayList<>();
+
+        if (sort[0].contains(",")) {
+            // will sort more than 2 fields
+            // sortOrder="field, direction"
+            for (String sortOrder : sort) {
+                String[] _sort = sortOrder.split(",");
+                orders.add(new Order(Sort.Direction.valueOf(sort[1].toUpperCase()), _sort[0]));
+            }
+        } else {
+            // sort=[field, direction]
+            orders.add(new Order(Sort.Direction.valueOf(sort[1].toUpperCase()), sort[0]));
+        }
+
+        Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders));
+
+        Page<User> users = null;
+        if (id != null){
+            users = userRepository.findById(id, pagingSort);
+        } else if (username != null){
+            users = userRepository.findByUsername(username, pagingSort);
+        } else if (phoneNumber != null) {
+            users = userRepository.findByPhoneNumber(phoneNumber, pagingSort);
+        } else if (address.equals("_") && name.equals("_")){
+            users = userRepository.findAll(pagingSort);
+        } else {
+            System.out.println(name);
+            users = userRepository.findByNameContainingOrAddressContaining(name, address, pagingSort);
+        }
+
+        if(users != null){
+            return users;
+        }
+
+        return null;
+    }
+
+    public Page<User> getAccountByCondition(String name, String address, Pageable pageable){
+        return userRepository.findByNameContainingOrAddressContaining(name, address, pageable);
+    }
+
+    public User updateUser(Long id, User request){
+        Optional<User> userData = userRepository.findById(id);
+
+        if(!userData.isPresent()){
+            return null;
+        }
+
+        User user = userData.get();
+
+        user.setUsername(request.getUsername());
+        user.setName(request.getName());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setAddress(request.getAddress());
+
+        User _user = userRepository.save(user);
+
+        return _user;
+    }
+
+    public void deleteUser (Long id){
+        Optional<User> userData = userRepository.findById(id);
+        userRepository.delete(userData.get());
+    }
+}
