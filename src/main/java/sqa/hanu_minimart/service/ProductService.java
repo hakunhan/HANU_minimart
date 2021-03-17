ffaae89a26ab814cd90c48d53a600784d98236a9
hakunhan <hakunhan@gmail.com>
2021-03-17 20:16:12 +0700

    Update product service

--- src/main/java/sqa/hanu_minimart/service/ProductService.java
+++ src/main/java/sqa/hanu_minimart/service/ProductService.java
@@ -129,7 +129,6 @@ public class ProductService {
 
         if(name.length() > 0 && !Objects.equals(name, product.getName())){
             productRepository.updateName(product.getName(), name);
-            return;
         }
 
         if(quantity > 0 && quantity != product.getQuantity()){
@@ -138,41 +137,34 @@ public class ProductService {
 
         if(price > 0 && price != product.getPrice()){
             productRepository.updatePrice(product.getName(), price);
-            return;
         }
 
         if(category.length() > 0 && !Objects.equals(category, product.getCategory())){
             productRepository.updateCategory(product.getName(), category);
-            return;
         }
 
         if(description.length() > 0 &&!Objects.equals(description, product.getDescription())){
             productRepository.updateDescription(product.getName(), description);
-            return;
         }
 
         if(picture_URL.length() > 0 &&!Objects.equals(picture_URL, product.getPicture_URL())){
             productRepository.updatePictureURL(product.getName(), picture_URL);
-            return;
         }
 
         if(sale >= 0){
             if (sale > 0 && product.getSale() == 0){
                 productRepository.updatePrice(product.getName(),product.getPrice() * sale / 100);
-            }else if(sale == 0 && product.getSale() > 0){
+            }else if(sale == 0 && product.getSale() != null && product.getSale() > 0){
                 productRepository.updatePrice(product.getName(),product.getPrice() / product.getSale() * 100);
             }
 
             productRepository.updateSale(product.getName(), sale);
-            return;
         }
 
         if(status.equalsIgnoreCase("hot")){
             productRepository.updateStatus(product.getName(), ProductStatus.HOT.toString());
-            return;
         }else if (status.equalsIgnoreCase("new")){
             productRepository.updateStatus(product.getName(), ProductStatus.NEW.toString());
-            return;
         }
 
         productRepository.save(product);
